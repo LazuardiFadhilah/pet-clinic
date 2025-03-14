98883f3ddfa5c37d706f21eb42fcf77cf1aa2fb1
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2025-03-14 21:51:05 +0700

    Remove unused FiArrowUpRight import in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import EquipmentSection from "./equipmentSection";
 import CharitySection from "./charitySection";
 import ReviewSection from "./reviewSection";
 import AppointmentSection from "./appointmentSection";
-import { FiArrowUpRight } from "react-icons/fi";
 
 export default function Home() {
   return (
